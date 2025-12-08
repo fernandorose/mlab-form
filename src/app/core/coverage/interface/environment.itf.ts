e9e7c86420a03db81d48ca184547f648a2498487
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-12-08 08:16:30 -0600

    🌟 | 12.MLAB feat: add EnvironmentItf interface and update index exports

---
+++ src/app/core/coverage/interface/environment.itf.ts
@@ -0,0 +1,5 @@
+export interface EnvironmentItf {
+  production: boolean;
+  api_url: string;
+  portal_url: string;
+}
--- src/app/core/coverage/interface/index.ts
+++ src/app/core/coverage/interface/index.ts
@@ -1,2 +1,3 @@
+export * from './environment.itf';
 export * from './http.itf';
 export * from './mlab.itf';
